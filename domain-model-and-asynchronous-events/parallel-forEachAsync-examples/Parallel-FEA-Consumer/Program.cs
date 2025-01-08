1d90e3e7ce40e5711688d02e2fbc8765b528b405
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2025-01-08 16:30:52 -0300

    consumindo e mostrando mensagens com sucesso

--- domain-model-and-asynchronous-events/parallel-forEachAsync-examples/Parallel-FEA-Consumer/Program.cs
+++ domain-model-and-asynchronous-events/parallel-forEachAsync-examples/Parallel-FEA-Consumer/Program.cs
@@ -7,16 +7,15 @@ var config = new ConfigurationBuilder()
                  .Build();
 
 var topics = config["Topics"].Split("|").ToList();
-var consumerList = new List<IConsumer<int, string>>();
+var consumerList = new List<IConsumer<string, string>>();
 
 topics.ForEach(topic =>
 {
-    var consumer = new ConsumerBuilder<int, string>(new ConsumerConfig()
+    var consumer = new ConsumerBuilder<string, string>(new ConsumerConfig()
     {
         GroupId = config["ConsumerConfig:GroupId"],
         BootstrapServers = config["ConsumerConfig:BootstrapServers"],
         AllowAutoCreateTopics = true,
-
     }).Build();
     consumer.Subscribe(topic);
     consumerList.Add(consumer);
@@ -37,11 +36,19 @@ await Parallel.ForEachAsync(consumerList, options, async (s, t) =>
     {
         await Task.Factory.StartNew(() =>
         {
-            Console.WriteLine($"Consuming {s.Subscription.First()}...");
-            s.Consume(t);
+            Console.WriteLine($"Consuming topic: {s.Subscription.First()}...");
+            try
+            {
+                var result = s.Consume(t);
+                if (result != null)
+                {
+                    Console.WriteLine($"Key: {result.Message.Key}, Value: {result.Message.Value}");
+                }
+            }
+            catch (ConsumeException ex)
+            {
+                Console.WriteLine($"Error consuming message: {ex.Error.Reason}");
+            }
         });
     }
 });
-
-
-
